a8b37ebe4a984064a4df6488ceecb450e7e3d865
dvelazcom <60010435+dvelazcom@users.noreply.github.com>
2025-10-25 02:18:19 -0700

    Delete dist directory

--- dist/pdf.worker.js
+++
@@ -1,2 +0,0 @@
-// PDF.js worker - will be loaded from CDN
-// The actual worker will be loaded from unpkg CDN at runtime
--- dist/tesseract-worker.js
+++
@@ -1,2 +0,0 @@
-// Tesseract.js worker files - these will be loaded from CDN
-// The actual files will be loaded from unpkg CDN at runtime
